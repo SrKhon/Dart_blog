c36191b5d886481f1e4dc8bad19e23d29f8f7f7e
serg <KhonSU>
2022-05-18 13:09:56 +0900

    init

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import Home, PostsByCategory, GetPost, PostsByTag
+
+urlpatterns = [
+    path('', Home.as_view(), name='home'),
+    path('category/<str:slug>', PostsByCategory.as_view(), name='category'),
+    path('tag/<str:slug>', PostsByTag.as_view(), name='tag'),
+    path('post/<str:slug>', GetPost.as_view(), name='post'),
+]
